cda0ccffaf56a67279b24d9b844238bd6ced5e2d
marie Fiombonana <marie.ahthan@gmail.com>
2022-04-10 14:43:58 +0300

    fix:import module

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,6 @@
-const app = require("app/app");
-const config = require("app/app.config");
-const database = require("app/app.database");
+const app = require("./app/app");
+const config = require("./app/app.config");
+const database = require("./app/app.database");
 
 console.log("Starting application ...");
 
