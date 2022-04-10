d6f69b890bcc8d1e7560a68869a4cd4798636d4a
marie Fiombonana <marie.ahthan@gmail.com>
2022-04-10 14:39:38 +0300

    fix:import module

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,9 @@
-import { app } from "./app/app";
-import { config } from "./app/app.config";
-import { database } from "./app/app.database";
+const app = require("./app/app");
+const config = require("./app/app.config");
+const database = require("./app/app.database");
 
 console.log("Starting application ...");
 
-database.connect(async () => {
-  const srv = app.init(config.server.port);
+database.database.connect(async () => {
+  const srv = app.app.init(config.config.server.port);
 });
