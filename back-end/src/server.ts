eb0a9bddef6f3821aded0996ec153f9985eb362b
marie Fiombonana <marie.ahthan@gmail.com>
2022-04-10 18:06:39 +0300

    import3

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,6 +1,9 @@
 import { app } from "./app/app";
+// var app = require('./app/app');
 import { config } from "./app/app.config";
+// var config = require('./app/app.config');
 import { database } from "./app/app.database";
+// var database = require('./app/app.database');
 
 console.log("Starting application ...");
 
