bfe799918c7d39a5d26d3cc22b268e1137dd296c
Derajck <56160845+Derajck@users.noreply.github.com>
2022-04-11 11:19:11 +0300

    asda

--- back-end/src/app/app.database.ts
+++ back-end/src/app/app.database.ts
@@ -1,39 +1,38 @@
 import mongoose from "mongoose";
 import { config } from "./app.config";
-class Database {
-  async connect(cb: () => void) {
-    const connexionString = config.mongodb.dbURI;
-    console.log("Trying to connect to database..");
-    mongoose
-      .connect(connexionString)
-      .then(() => {
-        console.log("Successfully connected to database");
-      })
-      .catch((error) => {
-        console.log("database connection failed. exiting now...");
-        console.error(error);
-        process.exit(1);
-      });
 
-    // mongoose.connection.on("connected", () => {
-    //   console.log(`connected to database: ${connexionString}`);
-    // });
-
-    mongoose.connection.once("open", () => {
-      cb();
-      console.log(`Database opened: ${connexionString}`);
+exports.Database = async function connect(cb: () => void) {
+  const connexionString = config.mongodb.dbURI;
+  console.log("Trying to connect to database..");
+  mongoose
+    .connect(connexionString)
+    .then(() => {
+      console.log("Successfully connected to database");
+    })
+    .catch((error) => {
+      console.log("database connection failed. exiting now...");
+      console.error(error);
+      process.exit(1);
     });
 
-    mongoose.connection.on("error", () => {
-      throw new Error(
-        `unable to connect to database: ${connexionString}`,
-      );
-    });
+  // mongoose.connection.on("connected", () => {
+  //   console.log(`connected to database: ${connexionString}`);
+  // });
 
-    mongoose.connection.on("disconnected", () => {
-      console.log(`Disconnected to database: ${connexionString}`);
-    });
-  }
-}
+  mongoose.connection.once("open", () => {
+    cb();
+    console.log(`Database opened: ${connexionString}`);
+  });
 
-export const database = new Database();
+  mongoose.connection.on("error", () => {
+    throw new Error(
+      `unable to connect to database: ${connexionString}`,
+    );
+  });
+
+  mongoose.connection.on("disconnected", () => {
+    console.log(`Disconnected to database: ${connexionString}`);
+  });
+};
+
+// export const database = new Database();
--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,12 +1,13 @@
+// @ts-ignore
 import { app } from "./app/app";
-// var app = require('./app/app');
+// var app = require("./app/app");
 import { config } from "./app/app.config";
-// var config = require('./app/app.config');
-import { database } from "./app/app.database";
-// var database = require('./app/app.database');
+// var config = require("./app/app.config");
+// import { database } from "./app/app.database";
+var database = require("./app/app.database");
 
 console.log("Starting application ...");
 
-database.connect(async () => {
+database.Database(async () => {
   const srv = app.init(config.server.port);
 });
