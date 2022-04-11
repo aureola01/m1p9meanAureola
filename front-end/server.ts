9d660c1bec7784379151f18481065f26c24508de
Derajck <56160845+Derajck@users.noreply.github.com>
2022-04-11 14:32:18 +0300

    Angular

---
+++ front-end/server.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import { Server } from 'http';
+import * as path from 'path';
+
+export class App {
+  private readonly app: express.Application;
+  constructor() {
+    this.app = express();
+    this.init(4200);
+  }
+
+  public init(port: number): Server {
+    this.app.use(express.static('dist/front-end'));
+    this.app.get('/*', (req: any, res: any) =>
+      res.sendFile(path.join(__dirname, 'dist/front-end/index.html'))
+    );
+    this.app.get('/', (req: any, res: any) =>
+      res.sendFile(path.join(__dirname, 'dist/front-end/index.html'))
+    );
+    return this.app.listen(process.env['PORT'] || port, () => {
+      console.log(`app started,listening on port ${port}`);
+    });
+  }
+}
+
+export const app = new App();
